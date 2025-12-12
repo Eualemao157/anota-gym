eba7e20895ecdf4ebb03953c4246111a217215e9
Alemao Gym <alemaogym@exemplo.com>
2025-12-12 01:44:22 -0300

    Aumentar limite do chunk e config vite

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 3000, // Aumenta o limite para 3000kb para não dar aviso
+  },
 })
